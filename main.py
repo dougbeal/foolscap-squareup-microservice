d7e2f495cb8fd48f39523b1de09abb8a99db887b
Douglas Beal <public.git@dougbeal.com>
2020-01-16 19:20:29 -0800

    Enable google Stackdriver logging

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from pprint import pformat
 
+
+import google.cloud.logging
+
+# Instantiates a client
+client = google.cloud.logging.Client()
+
+# Connects the logger to the root logging handler; by default this captures
+# all logs at INFO level and higher
+client.setup_logging()
+
 log = logging.getLogger()
 
 def foolscap_square_webhook(request):
-    log.debug(pformat(request))
+    log.info(pformat(request))
 
 def foolscap_tito_webhook(request):
-    log.debug(pformat(request))
+    log.info(pformat(request))
