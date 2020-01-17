9480aff2a008f6a2cb8d3bb9c406ba5288393259
Douglas Beal <public.git@dougbeal.com>
2020-01-16 19:15:43 -0800

    log reuqests

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from pprint import pformat
+
+log = logging.getLogger()
 
 def foolscap_square_webhook(request):
-    pass
+    log.debug(pformat(request))
 
 def foolscap_tito_webhook(request):
-    pass
+    log.debug(pformat(request))
