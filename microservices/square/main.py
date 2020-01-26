cb56a7f34686114bd3bc652f6a6421d5838f4fee
Douglas Beal <public.git@dougbeal.com>
2020-01-26 11:21:55 -0800

    consume command line options

--- microservices/square/main.py
+++ microservices/square/main.py
@@ -19,15 +19,15 @@ def set_webhook(level=logging.WARNING):
         if not PRODUCTION:
             import pdb, traceback
             traceback.print_exc()
-            pdb.post_mortem()    
-    
+            pdb.post_mortem()
+
 def get_registrations(level=logging.WARNING):
     logging.basicConfig(level=level)
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(api.get_registrations(secrets=secrets, client=client))
     except:
-        if not PRODUCTION:        
+        if not PRODUCTION:
             import pdb, traceback
             traceback.print_exc()
             pdb.post_mortem()
@@ -35,20 +35,42 @@ def get_registrations(level=logging.WARNING):
 if __name__ == '__main__':
     PRODUCTION = False
     import fire
-    import sys    
+    import sys
     from .. import development_config as config
     secrets = config.secrets
     client = config.SQUARE_CLIENT
-    if not '--production' in sys.argv:
-        from unittest.mock import patch
-        from unittest.mock import Mock 
-        from unittest.mock import MagicMock   
+    dry_run = True
+    if '--mode-production' in sys.argv:
+        sys.argv.remove('--mode-production')
+        #api.TITO_MODE = 'production'
+        dry_run = False
+    elif '--mode-production-dry-run' in sys.argv:
+        sys.argv.remove('--mode-production-dry-run')
+        #api.TITO_MODE = 'production'
+        dry_run = True
+    elif '--mode-test' in sys.argv:
+        sys.argv.remove('--mode-test')
+        #api.TITO_MODE = 'test'
+        dry_run = False
+    else:
+        if '--mode-test-dry-run' in sys.argv:
+            sys.argv.remove('--mode-test-dry-run')
+        #api.TITO_MODE = 'test'
+        dry_run = True
 
-        @patch('requests.post', MagicMock(side_effect=Mock(status_code=200, json=lambda : {"data": {"id": "test"}}))) 
-        @patch('requests.patch', MagicMock(side_effect=Mock(status_code=200, json=lambda : {"data": {"id": "test"}}))) 
+    if dry_run:
+        print("running dry, with mocked requests.post, requests.patch, and requests.delete")
+        #print("TITO_MODE " + api.TITO_MODE)
+        from unittest.mock import patch
+        from unittest.mock import Mock
+        from unittest.mock import MagicMock
+
+        @patch('requests.delete', MagicMock(side_effect=Mock(status_code=200, json=lambda : {"data": {"id": "test"}})))
+        @patch('requests.post', MagicMock(side_effect=Mock(status_code=200, json=lambda : {"data": {"id": "test"}})))
+        @patch('requests.patch', MagicMock(side_effect=Mock(status_code=200, json=lambda : {"data": {"id": "test"}})))
         def mocked_function():
-            fire.Fire()
+            return fire.Fire()
         mocked_function()
     else:
-        sys.argv.remove('--production')        
+        #print("TITO_MODE " + api.TITO_MODE)
         fire.Fire()
