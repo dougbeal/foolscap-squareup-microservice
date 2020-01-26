664d6c150463606e1a42724ab77fdfe7fb0b4114
Douglas Beal <public.git@dougbeal.com>
2020-01-26 11:22:29 -0800

    don't instantiate google logging in local mode

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from pprint import pformat
+import os
 
+if os.getenv('GAE_ENV', '').startswith('standard'):
+    import google.cloud.logging
+    from google.cloud import firestore
 
-import google.cloud.logging
-from google.cloud import firestore
+    # Instantiates a client
+    client = google.cloud.logging.Client()
 
-# Instantiates a client
-client = google.cloud.logging.Client()
-
-# Connects the logger to the root logging handler; by default this captures
-# all logs at INFO level and higher
-client.setup_logging()
+    # Connects the logger to the root logging handler; by default this captures
+    # all logs at INFO level and higher
+    client.setup_logging()
 
 import logging
 log = logging.getLogger()
-
+ 
 def foolscap_square_webhook(request):
     log.info("%s %s", pformat(request), pformat(request.get_data()))
 
