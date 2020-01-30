a2c94516a05c093736594c7c30744e135d8914e1
Douglas Beal <public.git@dougbeal.com>
2020-01-29 19:29:55 -0800

    inclucde origin attribute in pubsub message

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def foolscap_square_webhook(request):
                                       'square.change')
 
      # data must be a bytestring.
-    future = publisher.publish(topic_path, data="".encode("utf-8"))
+    future = publisher.publish(topic_path, origin="webhook")
     log.info("%s %s: %s", request, request.get_data(), future.result())
 
 def foolscap_tito_webhook(request):
